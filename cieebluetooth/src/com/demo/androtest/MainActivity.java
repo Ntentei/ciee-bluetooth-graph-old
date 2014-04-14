b6e464750ac3b6ae3fd8aa8641e92824351807b2
walker <qiangd6@gmail.com>
2014-04-14 23:27:41 +0800

    Output recevied data to console
    Console auto scroll to bottom and remove very old entries on top

--- cieebluetooth/src/com/demo/androtest/MainActivity.java
+++ cieebluetooth/src/com/demo/androtest/MainActivity.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Timer;
 import java.util.UUID;
 
 import org.apache.http.HttpEntity;
@@ -19,15 +20,21 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import com.demo.androtest.R;
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -38,6 +45,29 @@ import android.content.IntentFilter;
 public class MainActivity extends Activity {
 	
 	private BluetoothAdapter mBluetoothAdapter;
+	TextView text;
+	ScrollView scroll;
+	String xData;
+	String xxxData;
+	
+	
+	private static int countLines(String str){
+		   String[] lines = str.split("\r\n|\r|\n");
+		   return  lines.length;
+		}
+
+	
+	protected void updateConsole(String newLine) {
+		String aText = (String) text.getText();
+    	
+    	int lineNumber = countLines(aText);
+    	if(lineNumber >= 150)
+    		aText = aText.substring(aText.indexOf('\n')+1);
+    	
+    	aText = aText + "\n" + newLine;
+        text.setText(aText);
+		scroll.fullScroll(View.FOCUS_DOWN);  
+	}
 	
 
 	@Override
@@ -45,22 +75,28 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
+		
+		scroll = (ScrollView) findViewById(R.id.scroll1);
+		text = (TextView) findViewById(R.id.textView1);
 	
         //final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         
         if (mBluetoothAdapter == null) {
         	Log.e("STATE", "NO BLUETOOTH DEVICE ON-BOARD");
+        	updateConsole( "NO BLUETOOTH DEVICE ON-BOARD") ;
             return;
         }
         else
         {
 
         	Log.e("STATE", "Bluetooth initiated");
+        	updateConsole("Bluetooth initiated") ;
         	
         	if (!mBluetoothAdapter.isEnabled())
         	{
         		Log.e("STATE", "Bluetooth not enabled");
+        		updateConsole("Bluetooth not enabled") ;
         		
         	    if(!mBluetoothAdapter.isEnabled())
         	    {
@@ -72,6 +108,7 @@ public class MainActivity extends Activity {
         	{
 
         		Log.e("STATE", "Bluetooth enabled");
+        		updateConsole("Bluetooth enabled");
         		       		
         	    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         	        		
@@ -79,6 +116,7 @@ public class MainActivity extends Activity {
         		if (pairedDevices.size() > 0) {
         	
         			Log.e("STATE", "Paired device(s) found");
+        			updateConsole("Paired device(s) found");
         			
         			BluetoothDevice xDevice = null;
         			
@@ -92,31 +130,30 @@ public class MainActivity extends Activity {
 					try {
 						btSocket = xDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 
 					Log.e("STATE", "Socket created");
+					updateConsole("Socket created");
         		    
         		    try {
 						btSocket.connect();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
         		    
         		    Log.e("STATE", "Socket connected");
-       		    
+        		    updateConsole("Socket connected");
         		    
         		    InputStream input = null;
 					try {
 						input = btSocket.getInputStream();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					
 					Log.e("STATE", "InputStream acquired");
+					updateConsole("InputStream acquired");
 					
 		            int read = 0;
 		            byte[] singleChar = new byte[1];
@@ -130,10 +167,11 @@ public class MainActivity extends Activity {
 		                	String data = new String(singleChar, 0, read);
 		                	
 		                	Log.e("METADATA", data);
+		                	updateConsole(data);
 		                	
 		                	if(data.equals("~"))
 		                	{
-		                		String xData = "";
+		                		xData = "";
 		                		
 		                		Log.e("XXX", "**************BEGIN****************");
 		                		
@@ -173,9 +211,44 @@ public class MainActivity extends Activity {
 			                	data = new String(singleChar, 0, read);
 			                	xData += data;
 			                	
+			                	xxxData = xData;
+			                	
+					            //send data to database
+					            Thread thread = new Thread()
+					            {
+					                @Override
+					                public void run() {
+					                    
+							            Log.e("MSG", "Start HTTP GET in a new thread");
+							            updateConsole("Sending to Cloud Server...");						            
+					                	
+										HttpClient client = new DefaultHttpClient();  
+										String getURL = "http://192.168.1.104/newevent.php?deviceid=6635&value=" + xxData;
+										HttpGet get = new HttpGet(getURL);
+										try {
+											HttpResponse responseGet = client.execute(get);
+										    HttpEntity resEntityGet = responseGet.getEntity();  
+										    if (resEntityGet != null) {  
+										        String response = EntityUtils.toString(resEntityGet);
+										        Log.e("GET RESPONSE", response);
+										    }
+										} catch (ClientProtocolException e) {
+											e.printStackTrace();
+										} catch (IOException e) {
+											e.printStackTrace();
+										}
+					                }
+					            };
+					            
+					            thread.start();
+			                	
+					            //GUI update
 			                	Log.e("DATA", xData); //sample output: 01810xxxx
+			                	updateConsole(xData + "<--------------- Data");
 			                	
 			                	Log.e("XXX", "**************END******************");
+			                				                	
+
 		                	}
 
 		                }
@@ -186,110 +259,11 @@ public class MainActivity extends Activity {
 		            }
 		            while (read > 0);
 		         
-		            
-
-		            ///set data to database
-		            Thread thread = new Thread()
-		            {
-		                @Override
-		                public void run() {
-		                    
-				            Log.e("MSG", "Start HTTP GET in a new thread");
-		                	
-							HttpClient client = new DefaultHttpClient();  
-							String getURL = "http://192.168.1.104/newevent.php?deviceid=6635&value=change88xx";
-							HttpGet get = new HttpGet(getURL);
-							try {
-								HttpResponse responseGet = client.execute(get);
-							    HttpEntity resEntityGet = responseGet.getEntity();  
-							    if (resEntityGet != null) {  
-							        String response = EntityUtils.toString(resEntityGet);
-							        Log.e("GET RESPONSE", response);
-							    }
-							} catch (ClientProtocolException e) {
-								e.printStackTrace();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-		                }
-		            };
-		            
-		            thread.start();
-		            
-	            
-		            ///backup code of BT communication-------------------------------------------------------------------------
-		            
-					/*
-					 * 
-					 *
-					 * APPROACH B
-					 * 
-					 * 
-					
-					Log.e("STATE", "InputStream acquired");
-					
-        		    DataInputStream dinput = new DataInputStream(input);
-        		    
-        		    Log.e("STATE", "DataInputStream acquired");
-        		    
-        		    byte[] buf = new byte[8192];
-        		    int nread;
-        		    try {
-						while ((nread = dinput.read(buf)) >= 0) {
-						  Log.e("DATA", buf.toString());
-						}
-						
-						Log.e("DATA", "no data");
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					*/
-        		    
-        		    /*
-        		     * 
-        		     * 
-        		     * APPROACH C
-        		     * 
-        		     * 
-        		     
-        		    BufferedReader r = new BufferedReader(new InputStreamReader(input));
-        		    StringBuilder total = new StringBuilder();
-        		    String line;
-        		    try {
-						while ((line = r.readLine()) != null) {
-						    //total.append(line);
-							Log.e("XXX", line);
-						}
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					*/
-        		    
-        		    /*
-        		     * 
-        		     * 
-        		     * APPROACH D
-        		     * 
-        		     * 
-        		     * 
-        		    
-        		    byte[] byteArray = new byte[1024];
-					try {
-						dinput.readFully(byteArray, 0, byteArray.length);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					*/
-
-
         		}
         		else
         		{
         			Log.e("STATE", "No paired device(s)");
+        			updateConsole("No paired device(s)");
         		}
         		
         		
